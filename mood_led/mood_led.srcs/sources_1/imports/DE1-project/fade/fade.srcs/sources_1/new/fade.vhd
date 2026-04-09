cd08eb739e7921fad665b494190e47242ac61cbf
Richard Kralovsky <270829@vutbr.cz>
2026-04-09 17:05:29 +0200

    Inicializace top levelu - signaly + komponenty

---
+++ mood_led/mood_led.srcs/sources_1/imports/DE1-project/fade/fade.srcs/sources_1/new/fade.vhd
@@ -0,0 +1,83 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity fade is
+    Port ( ce    : in  STD_LOGIC;
+           clk   : in  STD_LOGIC;
+           rst   : in  STD_LOGIC;
+           r_val : out STD_LOGIC_VECTOR (7 downto 0);   -- 8 bit outputs -> 0 to 255
+           g_val : out STD_LOGIC_VECTOR (7 downto 0);
+           b_val : out STD_LOGIC_VECTOR (7 downto 0));
+end fade;
+
+architecture Behavioral of fade is
+    constant C_phase : integer := 255;                  -- how many steps are required for 1 phase
+    constant C_max   : integer := 6*C_phase;            -- maximum of internal counter
+    
+    signal col_cnt   : integer range 0 to C_max := 0;
+    signal sig_r     : integer range 0 to C_phase := 0; -- internal signals
+    signal sig_g     : integer range 0 to C_phase := 0;
+    signal sig_b     : integer range 0 to C_phase := 0;
+begin
+    color: process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst ='1' then                            -- reset
+                col_cnt <= 0;                           -- integers to 0
+                sig_r   <= 0;
+                sig_g   <= 0;
+                sig_b   <= 0;
+                
+                r_val   <= (others => '0');             -- all bits to 0
+                g_val   <= (others => '0');
+                b_val   <= (others => '0');
+                
+             elsif ce = '1' then 
+                if col_cnt >= C_max - 1 then            -- once full cycle is completed col_cnt goes from 0 again
+                    col_cnt <= 0;
+                else                                    -- increment of col_cnt
+                    col_cnt <= col_cnt + 1;
+                end if;
+                
+                if col_cnt <= C_phase then              -- red -> yellow
+                    sig_r <= C_phase;
+                    sig_g <= col_cnt;                   -- g++
+                    sig_b <= 0;
+                    
+                elsif col_cnt <= 2*C_phase then         -- yellow -> green
+                    sig_r <= 2*C_phase - col_cnt;       -- r--
+                    sig_g <= C_phase;
+                    sig_b <= 0; 
+                    
+                elsif col_cnt <= 3*C_phase then         -- green -> cyan
+                    sig_r <= 0;
+                    sig_g <= C_phase;
+                    sig_b <= col_cnt - 2*C_phase;       -- b++
+                    
+                elsif col_cnt <= 4*C_phase then         -- cyan -> blue
+                    sig_r <= 0;
+                    sig_g <= 4*C_phase - col_cnt;       -- g--
+                    sig_b <= C_phase;            
+                  
+                elsif col_cnt <= 5*C_phase then         -- blue -> magenta
+                    sig_r <= col_cnt - 4*C_phase;       -- r++
+                    sig_g <= 0;
+                    sig_b <= C_phase;
+                    
+                else                                    -- magenta -> red
+                    sig_r <= C_phase;
+                    sig_g <= 0;
+                    sig_b <= C_max - col_cnt;           -- b--
+                end if;
+             
+                -- final conversion to 8 bit vector with each ce signal
+                r_val <= std_logic_vector(to_unsigned(sig_r, 8)); 
+                g_val <= std_logic_vector(to_unsigned(sig_g, 8));
+                b_val <= std_logic_vector(to_unsigned(sig_b, 8));
+             end if;
+         end if;
+         
+    end process;
+
+end Behavioral;
--- mood_led/mood_led.srcs/sources_1/new/mood_led_top.vhd
+++ mood_led/mood_led.srcs/sources_1/new/mood_led_top.vhd
@@ -1,42 +1,75 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/02/2026 11:04:54 AM
--- Design Name: 
--- Module Name: mood_led_top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity mood_led_top is
---  Port ( );
+  port (
+        clk:  in std_logic;  
+        btnc: in std_logic; --reset
+        
+        btnu: in std_logic; --brigthness up
+        btnd: in std_logic; --brightness down
+        
+        btnr: in std_logic; --speed up
+        btnl: in std_logic; --speed down
+        
+        led17_r: out std_logic;
+        led17_g: out std_logic;
+        led17_b: out std_logic
+        );
 end mood_led_top;
 
 architecture Behavioral of mood_led_top is
 
+    component debounce is
+        Port ( clk       : in  STD_LOGIC;
+               rst       : in  STD_LOGIC;
+               btn_in    : in  STD_LOGIC;
+               btn_state : out STD_LOGIC;
+               btn_press : out STD_LOGIC);
+    end component debounce;
+    
+    component clk_en_dyn is
+        Port ( cnt_u : in  STD_LOGIC;
+               cnt_d : in  STD_LOGIC;
+               clk   : in  STD_LOGIC;
+               rst   : in  STD_LOGIC;
+               ce    : out STD_LOGIC);
+    end component clk_en_dyn;
+    
+    component fade is
+        Port ( ce    : in  STD_LOGIC;
+               clk   : in  STD_LOGIC;
+               rst   : in  STD_LOGIC;
+               r_val : out STD_LOGIC_VECTOR (7 downto 0);
+               g_val : out STD_LOGIC_VECTOR (7 downto 0);
+               b_val : out STD_LOGIC_VECTOR (7 downto 0));
+    end component fade;
+    
+    component clk_en is
+        generic ( G_MAX : positive );
+        port (
+            clk : in  std_logic;
+            rst : in  std_logic;
+            ce  : out std_logic
+        );
+    end component clk_en;
+    --OTHER COMPONENTS (bright_control, PWM)
+    
+    
+    signal s_brtup  : std_logic;
+    signal s_brtdw  : std_logic;
+    signal s_spup   : std_logic;
+    signal s_spdw   : std_logic;
+    
+    signal s_brt    : std_logic_vector (7 downto 0);
+    signal s_ce_dyn : std_logic;
+    signal s_ce     : std_logic;
+
+    signal s_r_val  : std_logic_vector (7 downto 0);
+    signal s_g_val  : std_logic_vector (7 downto 0);
+    signal s_b_val  : std_logic_vector (7 downto 0);
+    
 begin
 
 
