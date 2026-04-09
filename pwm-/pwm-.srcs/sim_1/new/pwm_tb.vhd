bbae32678d050ddd6b13a9a5b3ea69131b1f3d50
Ondrej-Kovar <270827@vutbr.cz>
2026-04-09 22:04:01 +0200

    Adding comments

--- pwm-/pwm-.srcs/sim_1/new/pwm_tb.vhd
+++ pwm-/pwm-.srcs/sim_1/new/pwm_tb.vhd
@@ -3,12 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity pwm_tb is
--- Testbench nemá porty
 end pwm_tb;
 
 architecture Behavioral of pwm_tb is
 
-    -- Komponenta, kterou testujeme (UUT - Unit Under Test)
+    -- tested component (UUT - Unit Under Test)
     component PWM_Module
         Port (
             clk      : in  STD_LOGIC;
@@ -19,19 +18,18 @@ architecture Behavioral of pwm_tb is
         );
     end component;
 
-    -- Signály pro propojení s UUT
+    -- signals for connecting UUT
     signal clk      : STD_LOGIC := '0';
     signal rst      : STD_LOGIC := '0';
     signal brt      : unsigned(7 downto 0) := (others => '0');
     signal val      : unsigned(7 downto 0) := (others => '0');
     signal led_pwr  : STD_LOGIC;
 
-    -- Definice periody hodin (např. 100 MHz -> 10 ns)
     constant clk_period : time := 10 ns;
 
 begin
 
-    -- Instance PWM modulu
+    -- Instancing PWM module
     uut: PWM_Module 
         port map (
             clk => clk,
@@ -41,7 +39,7 @@ begin
             led_pwr => led_pwr
         );
 
-    -- Generátor hodin
+    -- clk generator
     clk_process : process
     begin
         clk <= '0';
@@ -50,32 +48,31 @@ begin
         wait for clk_period/2;
     end process;
 
-    -- Stimulační proces
+    -- stimuli process
     stim_proc: process
     begin		
-        -- 1. Reset systému
+        -- system reset
         rst <= '1';
         wait for 50 ns;	
         rst <= '0';
         wait for clk_period * 10;
 
-        -- 2. Nastavení nízké střídy (např. 10 * 20 = 200 z 65535)
+        -- low duty_cycle (eg. 10 * 20 = 200 z 65535)
         brt <= to_unsigned(10, 8);
         val <= to_unsigned(20, 8);
         
-        -- Počkáme na začátek nového cyklu (65536 taktů)
-        -- V simulaci to trvá dlouho, tak počkáme jen na první náběh
+        -- waiting for new cycle (65536 ticks)
         wait until falling_edge(led_pwr);
 
-        -- 3. Změna jasu na 50% (128 * 256 = 32768, což je cca polovina z 65535)
+        -- 3. change to 50% (128 * 256 = 32768, roughly half of 65535)
         brt <= to_unsigned(128, 8);
         val <= to_unsigned(255, 8);
 
-        -- Necháme simulaci běžet dostatečně dlouho
+        -- keep simulation running
         wait for clk_period * 100000;
 
-        -- Zastavení simulace
-        report "Simulace dokoncena";
+        -- stop simulation
+        report "Simulation completed";
         wait;
     end process;
 
